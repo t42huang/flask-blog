510dec3ce083550499d996b7fa2665ef658c7f6f
Tina Huang <t42huang@gmail.com>
2020-02-17 08:02:29 -0500

    code restructure - use Jinja2 template engine to seperate presentation layer with business logic layer for easier maintainance

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    return render_template('index.html')
 
 @app.route('/hello/<name>')
 def hello(name):
-    return "Hello {}!".format(name)
+    return render_template('user.html', user=name)
 
 if __name__ == '__main__':
     app.run() 
