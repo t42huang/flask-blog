53437aa13b7cd47b6ff6e3b2320bfa3f3abb3ec9
Tina Huang <t42huang@gmail.com>
2020-02-28 08:57:01 -0500

    cache calculated md5 value to database to improve performance

--- app/models.py
+++ app/models.py
@@ -85,6 +85,7 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     @property
     def password(self):
@@ -163,11 +164,18 @@ class User(UserMixin, db.Model):
     
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
+        
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
+        
         if self.role is None:
             if self.email == current_app.config['FLASKBLOG_ADMIN']:
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
 
     def can(self, perm):
         return self.role is not None and self.role.has_permission(perm)
@@ -176,8 +184,11 @@ class User(UserMixin, db.Model):
         return self.can(Permission.ADMIN)
     
     def gravatar(self, size=100, default='identicon', rating='g'):
-        url = 'https://secure.gravatar.com/avatar'
-        hash = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or self.gravatar_hash()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating
         )
