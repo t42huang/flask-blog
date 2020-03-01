9f9438df58952f976bbddd7a444c5740e7bf59f1
Tina Huang <t42huang@gmail.com>
2020-02-29 19:23:01 -0500

    add post detail page with permalink

--- app/main/views.py
+++ app/main/views.py
@@ -32,6 +32,11 @@ def index():
     return render_template('index.html', form=form, posts=posts, 
         pagination=pagination)
 
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
+
 @main.route('/secret')
 @login_required
 def secret():
